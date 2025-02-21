3c6d86eabf59ce499a2aed65d7f3e3cf8a06c01b
valeriesanchr <valeriesahari@hotmail.com>
2025-02-20 22:12:50 -0600

    Lab1/src/Problema_3.py

--- Lab1/src/Problema_3.py
+++ Lab1/src/Problema_3.py
@@ -1,3 +1,12 @@
-lista = ["Pepito", "Pedrito", "Juanito", "Carlitos", "Fulanito","Menguanito"]
+lista_nombres = ["Pepito", "Pedrito", "Juanito", "Carlitos", "Fulanito","Menguanito"]
 
-print(lista[1])
+lista_sueldo_hora= [50,76,10,14,67,55]
+
+lista_horas_trabajadas = [4,8,9,5,1,7]
+
+for i in range(6):
+    
+    print(str(lista_nombres[i]) + " will be paid " + str(lista_horas_trabajadas[i]*lista_sueldo_hora[i])+ "$")
+    
+
+
